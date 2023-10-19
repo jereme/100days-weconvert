3fb7ddda61fdc33ee6464daaeeba2955b89a0954
Jereme Claussen <jereme.claussen@disneystreaming.com>
2023-10-19 10:40:37 -0700

    Initial Commit

---
+++ WeConvert/WeConvertApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeConvertApp.swift
+//  WeConvert
+//
+//  Created by Jereme Claussen on 10/19/23.
+//
+
+import SwiftUI
+
+@main
+struct WeConvertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
